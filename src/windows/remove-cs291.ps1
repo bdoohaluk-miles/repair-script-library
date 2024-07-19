23d909d1be94e290c419e39f1c2371dc046deaf7
bdoohaluk-miles <157630044+bdoohaluk-miles@users.noreply.github.com>
2024-07-19 07:16:50 -0400

    Update remove-cs291.ps1

--- src/windows/remove-cs291.ps1
+++ src/windows/remove-cs291.ps1
@@ -1,11 +1,16 @@
+# Initialize script
 . .\src\windows\common\setup\init.ps1
 . .\src\windows\common\helpers\Get-Disk-Partitions.ps1
 
-$partitionlist = Get-Disk-Partitions
+	# Handle disk partitions
+	$partitionlist = Get-Disk-Partitions
+	$partitionGroup = $partitionlist | Group-Object DiskNumber
+	$fixedDrives = $partitionGroup.Group | Select-Object -ExpandProperty DriveLetter
 
-forEach ( $partition in $partitionlist )
-{
-    $driveLetter = ($partition.DriveLetter + ":")
-    Get-Item $driveLetter +":\Windows\System32\drivers\CrowdStrike\C-00000291*.sys" | Remove-Item -Verbose
-}
-return $STATUS_SUCCESS
+	# Collect drive letters except for root drive of Rescue VM
+	$driveLetters += $fixedDrives
+
+        ForEach ($drive in $driveLetters ) {
+              Get-Item $drive + ":\Windows\System32\drivers\CrowdStrike\C-00000291*.sys" | Remove-Item -Verbose
+        }
+		
