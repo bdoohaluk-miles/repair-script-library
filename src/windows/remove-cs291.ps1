28d10c4954cdec7eeb4cf61b41481c472cc7d0d0
bdoohaluk-miles <157630044+bdoohaluk-miles@users.noreply.github.com>
2024-07-19 06:41:16 -0400

    Update remove-cs291.ps1

--- src/windows/remove-cs291.ps1
+++ src/windows/remove-cs291.ps1
@@ -1,1 +1,1 @@
-Get-Item ($drive)":\Windows\System32\drivers\CrowdStrike\C-00000291*.sys" | Remove-Item -Verbose
+Get-Item $drive +":\Windows\System32\drivers\CrowdStrike\C-00000291*.sys" | Remove-Item -Verbose
