2214552e6ae7856e60f60c00999aefe9cf03ac40
bdoohaluk-miles <157630044+bdoohaluk-miles@users.noreply.github.com>
2024-07-19 07:19:59 -0400

    Update remove-cs291.ps1

--- src/windows/remove-cs291.ps1
+++ src/windows/remove-cs291.ps1
@@ -11,6 +11,6 @@
 	$driveLetters += $fixedDrives
 
         ForEach ($drive in $driveLetters ) {
-              Get-Item $drive + ":\Windows\System32\drivers\CrowdStrike\C-00000291*.sys" | Remove-Item -Verbose
+              Get-Item ($drive + ":\Windows\System32\drivers\CrowdStrike\C-00000291*.sys)" | Remove-Item -Verbose
         }
 		
