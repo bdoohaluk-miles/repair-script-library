31b7157a4edaec77ae12405e161e822dd107781d
bdoohaluk-miles <157630044+bdoohaluk-miles@users.noreply.github.com>
2024-07-19 06:53:34 -0400

    Update remove-cs291.ps1

--- src/windows/remove-cs291.ps1
+++ src/windows/remove-cs291.ps1
@@ -1,1 +1,11 @@
-Get-Item $drive +":\Windows\System32\drivers\CrowdStrike\C-00000291*.sys" | Remove-Item -Verbose
+. .\src\windows\common\setup\init.ps1
+. .\src\windows\common\helpers\Get-Disk-Partitions.ps1
+
+$partitionlist = Get-Disk-Partitions
+
+forEach ( $partition in $partitionlist )
+{
+    $driveLetter = ($partition.DriveLetter + ":")
+    Get-Item $driveLetter +":\Windows\System32\drivers\CrowdStrike\C-00000291*.sys" | Remove-Item -Verbose
+}
+return $STATUS_SUCCESS
